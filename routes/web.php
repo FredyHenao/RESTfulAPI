23b70c179463a5014e9a6efa73b552a4160c46af
FredyHenao <jhongallego45y@hotmail.com>
2017-01-07 09:40:19 -0500

    Video 13
    Creando las rutas

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,6 @@
 | to using a Closure or controller method. Build something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('/', 'VehiculoController@showAll');
+Route::resource('fabricantes.vehiculos', 'VehiculoController');
+Route::resource('fabricantes', 'FabricantesController');
